4333519238c997045d672af1d9287b09075b9673
Jegors Čemisovs <yegor.chemisov@gmail.com>
2021-02-16 11:41:21 +0200

    Added interface

--- A7/factors-cpp/main.cpp
+++ A7/factors-cpp/main.cpp
@@ -1,6 +1,28 @@
 #include <iostream>
 
+void printFactors(int number);
+
+using namespace std;
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    cout << "The program prints factors of natural number." << endl;
+    int number;
+
+    do {
+        cout << "Enter a natural number (0 for exit):" << endl;
+        cin >> number;
+        if (number < 0) {
+            cout << "The number should be positive" << endl;
+            continue;
+        }
+        if (number > 0) {
+            printFactors(number);
+        }
+    } while (number != 0);
     return 0;
+}
+
+void printFactors(int number) {
+    cout << "Factors for " << number << ":";
+    cout << endl;
 }
