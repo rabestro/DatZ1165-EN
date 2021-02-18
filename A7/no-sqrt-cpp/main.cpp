9eff47f06f6fdc551cebdf49bcfba7f1e6eed9ec
Jegors Čemisovs <yegor.chemisov@gmail.com>
2021-02-18 13:30:13 +0200

    Code reformatted

--- A7/no-sqrt-cpp/main.cpp
+++ A7/no-sqrt-cpp/main.cpp
@@ -4,28 +4,29 @@ using namespace std;
 
 int main() {
     cout << "The program prints factors of natural number." << endl;
-    int number;
 
-    do {
+    for (;;) {
+        int number;
         cout << "Enter a natural number (0 for exit):" << endl;
         cin >> number;
         if (number < 0) {
             cout << "The number should be positive." << endl;
             continue;
         }
-        if (number > 0) {
-            for (int divisor = 1; divisor <= number / divisor; ++divisor) {
-                if (number % divisor == 0) {
-                    cout << divisor << ' ';
-                    const int paired_divisor = number / divisor;
-                    if (paired_divisor != divisor) {
-                        cout << paired_divisor << ' ';
-                    }
+        if (number == 0) {
+            break;
+        }
+        for (int divisor = 1; divisor <= number / divisor; ++divisor) {
+            if (number % divisor == 0) {
+                cout << divisor << ' ';
+                const int paired_divisor = number / divisor;
+                if (paired_divisor != divisor) {
+                    cout << paired_divisor << ' ';
                 }
             }
-            cout << endl;
         }
-    } while (number != 0);
+        cout << endl;
+    }
 
     return 0;
 }
