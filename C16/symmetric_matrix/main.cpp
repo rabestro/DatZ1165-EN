580de1b2ad85a22d33f3e3eec031b3f87c902dc0
Jegors Čemisovs <yegor.chemisov@gmail.com>
2021-02-21 22:53:20 +0200

    Solved C16

--- C16/symmetric_matrix/main.cpp
+++ C16/symmetric_matrix/main.cpp
@@ -1,27 +1,47 @@
 #include <iostream>
-#include <algorithm>
-#include <array>
 
 using namespace std;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    std::cout << "The program checks is given matrix symmetric." << endl;
 
     int size;
     for (;;) {
-        cout << "Enter a matrix size (0 for exit):" << endl;
+        cout << endl << "Enter size of square matrix (0 for exit):" << endl;
         cin >> size;
         if (size < 0) {
-            cout << "The size should be positive or zero." << endl;
+            cout << "The number should be positive or zero." << endl;
             continue;
         }
         if (size == 0) {
             break;
         }
         const int cells = size * size;
-        int Matrix[cells];
-        for (int i = 0; i < cells; ++i) cin >> Matrix[i];
+        int *matrix = new int(cells);
 
+        cout << "Enter " << cells << " numbers representing the square matrix:" << endl;
+
+        for (int i = 0; i < cells; ++i) cin >> matrix[i];
+
+        cout << "Is the matrix symmetric by ..." << std::boolalpha << endl;
+
+        bool main_diagonal = true;
+        bool side_diagonal = true;
+        bool horizontal = true;
+        bool vertical = true;
+
+        for (int i = 0; i < cells; ++i) {
+            main_diagonal &= matrix[i] == matrix[i / size + i % size * size];
+            side_diagonal &= matrix[i] == matrix[size * (size - i % size) - i / size - 1];
+            horizontal &= matrix[i] == matrix[size * (size - i / size - 1) + i % size];
+            vertical &= matrix[i] == matrix[size - i % size - 1 + i / size * size];
+        }
+        cout << "main diagonal: " << main_diagonal << endl;
+        cout << "side diagonal: " << side_diagonal << endl;
+        cout << "   horizontal: " << horizontal << endl;
+        cout << "     vertical: " << vertical << endl;
+
+        delete matrix;
     }
 
     return 0;
