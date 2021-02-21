b2976316688dd31bdce924a50e47b94a9dd8ec86
Jegors Čemisovs <yegor.chemisov@gmail.com>
2021-02-21 11:49:43 +0200

    Added function square_sum

--- B26/triples-cpp/main.cpp
+++ B26/triples-cpp/main.cpp
@@ -3,6 +3,17 @@
 
 using namespace std;
 
+/*
+ * The function calculates square sum of 3 numbers.
+ */
+int square_sum(const int x, const int y, const int z) {
+    return x * x + y * y + z * z;
+}
+
+/*
+ * The method prints all triples of numbers (x, y, z)
+ * where x^2+y^2+z^2 = n (if such triplets exist).
+ */
 void printTriples(const int n) {
     bool found = false;
     for (int x = sqrt(n); x >= 0; --x) {
@@ -12,7 +23,8 @@ void printTriples(const int n) {
             const int z = sqrt(rest);
             if (z * z == rest) {
                 found = true;
-                cout << '(' << x << ", " << y << ", " << z << ") " << endl;
+                cout << x << "^2 + " << y << "^2 + " << z << "^2 = ";
+                cout << square_sum(x, y, z) << endl;
             }
         }
     }
@@ -27,7 +39,7 @@ int main() {
 
     int number;
     for (;;) {
-        cout << "Enter a natural number (0 for exit):" << endl;
+        cout << endl << "Enter a natural number (0 for exit):" << endl;
         cin >> number;
         if (number < 0) {
             cout << "The number should be positive." << endl;
